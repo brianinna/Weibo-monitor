ce3b51fe4012fa405a97cb9c821b1dffd5e10a9a
brianinna <brianwadejing@gmail.com>
2026-05-17 20:35:45 +0800

    Fix noVNC websockify listen address

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ if [ "${WEIBO_MONITOR_ENABLE_VNC:-1}" != "0" ]; then
   Xvfb "${DISPLAY:-:99}" -screen 0 "${WEIBO_MONITOR_SCREEN:-1366x768x24}" -nolisten tcp >/tmp/xvfb.log 2>&1 &
   sleep 0.5
   x11vnc -display "${DISPLAY:-:99}" -forever -shared -nopw -rfbport 5900 >/tmp/x11vnc.log 2>&1 &
-  websockify --web=/usr/share/novnc/ 0.0.0.0 "${WEIBO_MONITOR_NOVNC_PORT:-18790}" localhost:5900 >/tmp/novnc.log 2>&1 &
+  websockify --web=/usr/share/novnc/ "0.0.0.0:${WEIBO_MONITOR_NOVNC_PORT:-18790}" localhost:5900 >/tmp/novnc.log 2>&1 &
 fi
 
 exec "$@"
